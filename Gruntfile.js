16b4985da1a0f19bb46ea3f2c323e90072e59519
tmpChris <chris@tmpcompany.com>
2015-05-18 08:20:30 -0400

    template update

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,37 +4,65 @@ module.exports = function(grunt) {
 	var d = new Date();
 	var obj = {
 		// Template
-		cdnLocation: 	'ajax.googleapis.com/ajax/libs/jquery',
+		cdnLocation: 	'string_partial',
 		trCoStart: 		'?utm_source=bm23&utm_medium=email&utm_content=',
 		trCoEnd: 		'&utm_campaign=!date-code-!slug&a=!date-code-!slug',
 		domainName: 	'example.com',
-		copyright: 		'this brand™ is a trademark of Some Legal Entity. © ' + d.getFullYear() + ' Some Legal Entity. All rights reserved.',
+		copyright: 		'© ' + d.getFullYear() + ' Some Legal Entity. Disclaimer.',
+
+		// Which html file(s)?
+		fileBottom: 	'dev/main-plus-six.html',
 
 		// Primary content
 		dateCode: 		'70-0101',
-		slug: 			'bikes',
-		landingPage: 	'/bikes',
+		slug: 			'string',
+		landingPage: 	'/string', //main banner link & social share link
 
 		// Single Incidence Content
 		expirationDate: 			'January 1, 1970 at 12:00 AM UTC',
-		exclusions: 				'Excludes sale items, previous purchases and drop ship items',
-		tweet: encodeURIComponent('#awesome deals for #NationalBikeMonth @TwitterDev http://!domain!landing-page'),
+		exclusions: 				'string',
+		tweet:   encodeURIComponent('#MyTweet @TwitterDev http://!domain!landing-page'),
 		
 		urls: {
-			featureOne: 		'/this-product-page',
-			featureTwo: 		'/that-product-page',
-			featureThr: 		'/what-product-page',
-			featureFou: 		'/ohmy-product-page'
+			featureOne: 	'/link_to',
+			featureTwo: 	'/link_to',
+			featureThr:		'/link_to',
+			featureFou: 	'/link_to',
+			sub1L:  		'/link_to',
+			sub1R:  		'/link_to',
+			sub2L:  		'/link_to',
+			sub2R:  		'/link_to',
+			sub3L:  		'/link_to',
+			sub3R: 			'/link_to'
 		},
 
 		altText: {
-			mainPromo: 				'Shop Today\'s best deal!',
-			feature1L: 				'Check out this great bicycle!',
-			feature1R: 				'Be sure not to miss this!',
-			featureOne: 			'Shop for this bicycle now!',
-			featureTwo: 			'Check out some Bike parts now!',
-			featureThr: 			'Look at our sale bikes now!',
-			featureFou: 			'Look at our sale bikes now!'
+			mainPromo: 	'string',
+			featureOne: 'string',
+			featureTwo: 'string',
+			featureThr: 'string',
+			featureFou: 'string',
+			sub1L: 		'string',
+			sub1R:  	'string',
+			sub2L:  	'string',
+			sub2R:  	'string',
+			sub3L:  	'string',
+			sub3R:  	'string'
+		}, 
+
+		news: {
+			head: 		'string',
+			sub: 		'string',
+			linkLocat: 	'link_to', //full link. Assumes (target != !domain). 
+			pone: 		'string',
+			ptwo: 		'string',
+			pthr: 		'string'
+		},
+
+		footnav: {
+			fn01: 	'link_to',
+			fn02: 	'link_to'
+			// TODO: all the way up to 15, then add to replace task.
 		}
 	};
 
@@ -47,7 +75,7 @@ module.exports = function(grunt) {
 				src: [
 					'dev/top.html', 
 					'dev/_temp/style.css',
-					'dev/bottom.html'
+					obj.fileBottom
 				],
 				dest: 'dev/_temp/master.html',
 			}
@@ -75,6 +103,17 @@ module.exports = function(grunt) {
 			scripts: {
 				files: ['dev/*.html', 'less/style.less'],
 				tasks: ['default']
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					collapseWhitespace: false
+				},
+				files: {
+					'index.html': 'inline.html',     // 'destination': 'source'
+				}
 			}
 		},
 		replace: {
@@ -121,12 +160,6 @@ module.exports = function(grunt) {
 					from: '!alt-main-promo',
 					to: obj.altText.mainPromo
 				}, {
-					from: '!alt-feature-1-L',
-					to: obj.altText.feature1L
-				}, {
-					from: '!alt-feature-1-R',
-					to: obj.altText.feature1R
-				}, {
 					from: '!alt-feature-1',
 					to: obj.altText.featureOne
 				}, {
@@ -150,7 +183,67 @@ module.exports = function(grunt) {
 				}, {
 					from: '!url-feature-fou',
 					to: obj.urls.featureFou
-				}]
+				}, {
+					from: '!sub-1-L',
+					to: obj.urls.sub1L
+				}, {
+					from: '!sub-1-R',
+					to: obj.urls.sub1R
+				}, {
+					from: '!sub-2-L',
+					to: obj.urls.sub2L
+				}, {
+					from: '!sub-2-R',
+					to: obj.urls.sub2R
+				}, {
+					from: '!sub-3-L',
+					to: obj.urls.sub3L
+				}, {
+					from: '!sub-3-R',
+					to: obj.urls.sub3R
+				}, {
+					from: '!alt-sub-1-L',
+					to: obj.altText.sub1L
+				}, {
+					from: '!alt-sub-1-R',
+					to: obj.altText.sub1R
+				}, {
+					from: '!alt-sub-2-L',
+					to: obj.altText.sub2L
+				}, {
+					from: '!alt-sub-2-R',
+					to: obj.altText.sub2R
+				}, {
+					from: '!alt-sub-3-L',
+					to: obj.altText.sub3L
+				}, {
+					from: '!alt-sub-3-R',
+					to: obj.altText.sub3R
+				}, {
+					from: '!news-headline',
+					to: obj.news.head
+				}, {
+					from: '!news-subhead',
+					to: obj.news.sub
+				}, {
+					from: '!news-link',
+					to: obj.news.linkLocat
+				}, {
+					from: '!news-pone',
+					to: obj.news.pone
+				}, {
+					from: '!news-ptwo',
+					to: obj.news.ptwo
+				}, {
+					from: '!news-pthr',
+					to: obj.news.pthr
+				}, {
+					from: '!footnav-01',
+					to: obj.footnav.fn01
+				}, {
+					from: '!footnav-02',
+					to: obj.footnav.fn02
+				} ]
 			}
 		}
 	});
@@ -161,9 +254,10 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-inline-css');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-text-replace');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
 	// 4. Tasks
-	grunt.registerTask('default', ['less', 'concat:dist', 'inlinecss', 'replace']);
+	grunt.registerTask('default', [ 'less', 'concat:dist', 'inlinecss', 'replace', 'htmlmin']);
 };
 
 
